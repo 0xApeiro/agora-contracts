4cc32dd914bf120b3d5256c2331aa864ed4dac14
Alan Le <underrated@gmail.com>
2022-09-10 17:23:48 -0700

    feat: deploy initial agora contract

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.15;
 
-import {Script} from 'forge-std/Script.sol';
+import { Script } from "forge-std/Script.sol";
 
-import {Greeter} from "src/Greeter.sol";
+import { LegacyWorks } from "src/LegacyWorks.sol";
 
 /// @notice A very simple deployment script
 contract Deploy is Script {
-
   /// @notice The main script entrypoint
-  /// @return greeter The deployed contract
-  function run() external returns (Greeter greeter) {
+  /// @return legacy The deployed contract
+  function run() external returns (LegacyWorks legacy) {
     vm.startBroadcast();
-    greeter = new Greeter("GM");
+    legacy = new LegacyWorks();
+    legacy.mint(10);
     vm.stopBroadcast();
   }
 }
